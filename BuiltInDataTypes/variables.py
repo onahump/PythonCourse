af06da5823a4a2c150af6e9dc4b882ed6b6ea4b9
Nahum Ponce <oswaldo.ponce@softtek.com>
2020-06-08 05:06:34 +0000

    Variables examples

---
+++ BuiltInDataTypes/variables.py
@@ -0,0 +1,7 @@
+my_name = "name"
+my_name # This'll return the value of the my_name variable
+print(my_name)
+my_name += " LastName"
+my_first_name = "Juan"
+my_last_name = " Perez"
+my_complete_name = my_firt_name + my_last_name
