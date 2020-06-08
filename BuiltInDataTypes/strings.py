976835e44440762205c37c309c8b22323b9e98af
Nahum Ponce <oswaldo.ponce@softtek.com>
2020-06-08 04:38:28 +0000

    Strings

---
+++ BuiltInDataTypes/strings.py
@@ -0,0 +1,20 @@
+# Documentation - https://docs.python.org/3/library/stdtypes.html#text-sequence-type-strl
+
+mystring = 'hello'
+
+mydoublequotestring = "Ohla" #This will return a single quote string
+
+mymultilinestring = '''Hello
+                       how are you
+                       Im fine'''
+
+another = 'Hi' + ' Name'
+
+ha = 'Ha' * 4
+
+strinmethod = "pancho".find('h')
+lowermethod = "TesTinG".lower()
+tabCharacter = "Tab\tDeLimited" #This is a scape character
+newLine = "Tab\nDeLimited" #This is a scape character
+useDoubleQuotes = '"Double" quotes'
+useQuotes = '\'Simple\' quotes'
