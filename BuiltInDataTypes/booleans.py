54f88c403879f018e796c698e5050fb552d5aade
Nahum Ponce <oswaldo.ponce@softtek.com>
2020-06-08 05:00:55 +0000

    Booleans examples

---
+++ BuiltInDataTypes/booleans.py
@@ -0,0 +1,9 @@
+nothing = None
+atruevalue = True
+afalsevalue = False
+value = bool(1.0) # this'll return True
+novalue = bool(0) # this'll return False
+stringnovalue = bool("") # This'll return False
+stringvalue = bool(" ") # This'll return True because this have somethis inside
+nuvaluenumber = bool(0.0) # This'll return False
+
