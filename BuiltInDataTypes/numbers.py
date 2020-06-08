38575c26e5703432a6e75a6dc27a3a1627c3ca7e
Nahum Ponce <oswaldo.ponce@softtek.com>
2020-06-08 04:52:21 +0000

    Numbers examples

---
+++ BuiltInDataTypes/numbers.py
@@ -0,0 +1,14 @@
+
+mysum = 2 + 2
+mydivision = 5 / 2
+mydivisionInteger = 5 // 2
+myreminder = 5 % 3
+myexponent = 2 ** 3
+myexponettwo = pow(2, 3)
+floatnumber = 1.2 + 3
+floatnumbertwo = 4.0 + 2
+stringnumber = str('7.0')
+fromstringtofloat = float('7.0')
+fromstringtoint = int('1')
+
+
